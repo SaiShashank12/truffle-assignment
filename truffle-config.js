493b4405e1332f95bfcc4093b7cedb72ba826c8e
Sai Shashank Mudliar <shashanksai.ss@gmail.com>
2022-10-21 03:37:40 -0400

    deployed testnet blockchain

--- truffle-config.js
+++ truffle-config.js
@@ -41,11 +41,12 @@
  * https://trufflesuite.com/docs/truffle/getting-started/using-the-truffle-dashboard/
  */
 
-// require('dotenv').config();
-// const { MNEMONIC, PROJECT_ID } = process.env;
+require('dotenv').config();
+const { MNEMONIC, PROJECT_ID } = process.env;
 
-// const HDWalletProvider = require('@truffle/hdwallet-provider');
-
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+// new HDWalletProvider(MNEMONIC,
+//   `https://nd-089-724-267.p2pify.com/a796f775ef3226e508093faf787fc903`)
 module.exports = {
   /**
    * Networks define how you connect to your ethereum client and let you set the
@@ -75,20 +76,27 @@ module.exports = {
     //   port: 8777,             // Custom port
     //   network_id: 1342,       // Custom network
     //   gas: 8500000,           // Gas sent with each transaction (default: ~6700000)
-    //   gasPrice: 20000000000,  // 20 gwei (in wei) (default: 100 gwei)
+    //   gasPrice: 20000000000,   // 20 gwei (in wei) (default: 100 gwei)
     //   from: <address>,        // Account to send transactions from (default: accounts[0])
     //   websocket: true         // Enable EventEmitter interface for web3 (default: false)
     // },
     //
     // Useful for deploying to a public network.
     // Note: It's important to wrap the provider as a function to ensure truffle uses a new provider every time.
-    // goerli: {
-    //   provider: () => new HDWalletProvider(MNEMONIC, `https://goerli.infura.io/v3/${PROJECT_ID}`),
-    //   network_id: 5,       // Goerli's id
-    //   confirmations: 2,    // # of confirmations to wait between deployments. (default: 0)
-    //   timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-    //   skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
-    // },
+    chainstack:{
+      provider:()=> new HDWalletProvider(MNEMONIC,"https://nd-642-344-431.p2pify.com/b8b326e3c11e7f13900d7261166138fa",0,10,
+      true, "m/44'/1'/0'/0/"),
+      network_id:"*",
+      from: "0xfFd2B33Fa9867C7cF8d00636A3a61375a634688A"
+    },
+    goerli: {
+      provider: () => {return new HDWalletProvider(MNEMONIC, `https://nd-356-376-875.p2pify.com/bf73790122bd28443e3494a63d630d4b`)},
+      network_id: 5,       // Goerli's id
+      confirmations: 2,    // # of confirmations to wait between deployments. (default: 0)
+      timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+    },
+    
     //
     // Useful for private networks
     // private: {
@@ -106,13 +114,13 @@ module.exports = {
   // Configure your compilers
   compilers: {
     solc: {
-      version: "0.8.17" // Fetch exact version from solc-bin (default: truffle's version)
+      version: "0.8.17", // Fetch exact version from solc-bin (default: truffle's version)
       // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
       // settings: {          // See the solidity docs for advice about optimization and evmVersion
-      //  optimizer: {
-      //    enabled: false,
-      //    runs: 200
-      //  },
+       optimizer: {
+         enabled: true,
+         runs: 200
+       },
       //  evmVersion: "byzantium"
       // }
     }
